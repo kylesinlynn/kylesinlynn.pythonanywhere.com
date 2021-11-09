47e0b321aa8c2b569c86968345c6fcc6ca3e4530
Kyle Sin Lynn <kylesinlynn@gmail.com>
2021-11-09 09:44:43 +0630

    changed language code to en-gb

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-uk'
+LANGUAGE_CODE = 'en-gb'
 
 TIME_ZONE = 'UTC'
 
