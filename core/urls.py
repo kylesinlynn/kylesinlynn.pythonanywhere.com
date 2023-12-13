679985230c2fa1482092b9138a83df937a5ea499
Kyle Sin Lynn <kylesinlynn@gmail.com>
2023-12-13 21:50:38 +0630

    feat: createinitial portfolio project

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('portfolio.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ portfolio/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+
+from portfolio import views
+
+app_name = "portfolio"
+urlpatterns = [
+    path('', views.index, name='index')
+]
