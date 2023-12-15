7f6c5c5aa3c548c64921fad54cbab05fe673ff40
Kyle Sin Lynn <kylesinlynn@gmail.com>
2023-12-15 19:33:58 +0630

    feat: create dynamic portfolio

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,46 +1,8 @@
 from django.shortcuts import render
-
+from portfolio.models import Profile
 # Create your views here.
 def index(request):
     context = {
-        'title': 'Kyle Sin Lynn',
-        'description': 'Kyle Sin Lynn, a dedicated software engineer, excels in Python and Django. Committed to innovation, problem-solving, and collaborative success.',
-        'keywords': [
-            'kylesinlynn',
-            'Kyle Sin Lynn'
-        ],
-        'image': 'https://avatars.githubusercontent.com/u/82741688?v=4',
-        'email': 'kylesinlynn@gmail.com',
-        'phone': '+959766107203',
-        'facebook': 'danielhannaing',
-        'x': 'kylesinlynn',
-        'github': 'kylesinlynn',
-        
-        'works': [
-                {
-                    'client': 'Microsoft Inc',
-                    'title': 'Management System',
-                    'description': 'Better communication and control workflow on time',
-                    'link': '#'
-                },
-                {
-                    'client': 'Microsoft Inc',
-                    'title': 'Management System',
-                    'description': 'Better communication and control workflow on time',
-                    'link': '#'
-                },
-                {
-                    'client': 'Microsoft Inc',
-                    'title': 'Management System',
-                    'description': 'Better communication and control workflow on time',
-                    'link': '#'
-                },
-                {
-                    'client': 'Microsoft Inc',
-                    'title': 'Management System',
-                    'description': 'Better communication and control workflow on time',
-                    'link': '#'
-                },
-            ],
+        'profile': Profile.objects.latest('id')
     }
     return render(request, 'portfolio/index.html', context)
