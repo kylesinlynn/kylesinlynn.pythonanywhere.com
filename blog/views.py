fc3bf34e80fcb7f59f46406853a30bdda055349d
Kyle Sin Lynn <kylesinlynn@gmail.com>
2023-12-16 18:51:07 +0630

    feat: implement infinite scroll for blog posts pagination

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from portfolio.models import Profile
@@ -6,9 +7,15 @@ from blog.models import Article
 profile = lambda _: Profile.objects.latest('id')
 
 def index(request):
+    articles = Article.objects.all()
+    page = request.GET.get("page") if 'page' in request.GET else 1
+    
+    paginator = Paginator(articles, 1)
+    blog = paginator.get_page(page)
+    
     context = {
         'profile': profile(1),
-        'blog': Article.objects.all()
+        'blog': blog
     }
     return render(request, 'blog/index.html', context)
 
