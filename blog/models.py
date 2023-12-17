88bbd0dc5f9e083b07de61012b5297c4a2235e7f
Kyle Sin Lynn <kylesinlynn@gmail.com>
2023-12-17 10:20:56 +0630

    feat: implement timestamps and modified status to articles

--- blog/models.py
+++ blog/models.py
@@ -1,17 +1,16 @@
 from django.utils.text import slugify 
 from django.db import models
 
-class Article(models.Model):
+from model_utils.models import TimeStampedModel
+
+class Article(TimeStampedModel):
     title = models.CharField(name="title", max_length=73, help_text="Enter the article title", null=True)
     description = models.TextField(name="description", help_text="Enter text", null=True)
     image = models.ImageField(name="image", help_text="Upload cover image", blank=True, null=True)
     slug = models.SlugField(name="slug", auto_created=True, unique=True, blank=True, null=True)
     
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True)
-    
     class Meta:
-        ordering = ('-created_at',)
+        ordering = ('-id',)
     
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
