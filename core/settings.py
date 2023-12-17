0e7fbe25c51703cc80db9a1acabf1a5c3752281e
Kyle Sin Lynn <kylesinlynn@gmail.com>
2023-12-17 16:45:14 +0630

    feat: prepare to deploy production

--- core/settings.py
+++ core/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^v*t#@vw$g4d+)+zhk9n!b%fya8vy!q(0#o08bykm6d_^9_gk-'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
